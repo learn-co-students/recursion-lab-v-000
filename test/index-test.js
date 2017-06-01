7fba168bd38ea18b3b8716477adeb1e95f5747ed
Gabe Jackson <gj@mail.co.de>
2017-06-01 17:31:49 -0400

    remove 'should' from test descriptions

--- test/index-test.js
+++ test/index-test.js
@@ -11,7 +11,7 @@ afterEach(function() {
 });
 
 describe('printString()', function() {
-  it("should print out all of the characters in a passed-in string", function() {
+  it("prints out all of the characters in a passed-in string", function() {
     printString('pizza');
 
     expect(console.log).toHaveBeenCalledWith("p");
@@ -33,7 +33,7 @@ describe('printString()', function() {
     expect(console.log).toHaveBeenCalledWith("e");
   });
 
-  it("uses recursion, calling itself once for each letter in the string", function() {
+  it("recurses, calling itself once for each letter in the string", function() {
     var printString = sinon.spy(window, "printString");
     printString("pizza");
     expect(printString.callCount).toEqual(5);
@@ -41,12 +41,12 @@ describe('printString()', function() {
 });
 
 describe('reverseString()', function() {
-  it("should reverse all of the letters of a string", function() {
+  it("reverses all of the letters in a string", function() {
     expect(reverseString('pizza')).toEqual("azzip");
     expect(reverseString('chocolate')).toEqual("etalocohc");
   });
 
-  it("should make the proper recursive calls", function() {
+  it("makes the proper recursive calls", function() {
     var reverseString = sinon.spy(window, "reverseString");
     reverseString("pizza");
     expect(reverseString.callCount).toEqual(5);
@@ -55,15 +55,15 @@ describe('reverseString()', function() {
 
 
 describe('isPalindrome()', function() {
-  it("should return false when a string is not a palindrome", function() {
+  it("returns 'false' when a string is not a palindrome", function() {
     expect(isPalindrome('pizza')).toEqual(false);
   });
 
-  it("should return true when a string is a palindrome", function() {
+  it("returns 'true' when a string is a palindrome", function() {
     expect(isPalindrome("madamimadam")).toEqual(true)
   });
 
-  it("should make the proper recursive calls", function() {
+  it("makes the proper recursive calls", function() {
     var isPalindrome = sinon.spy(window, "isPalindrome");
     isPalindrome("madamimadam")
     expect(isPalindrome.callCount).toEqual(6)
@@ -71,11 +71,11 @@ describe('isPalindrome()', function() {
 });
 
 describe('addUpTo()', function() {
-  it("should add up to a given index in an array", function() {
+  it("sums all members up to a given index in an array", function() {
     expect(addUpTo([1, 4, 5, 3], 2)).toEqual(10);
   });
 
-  it("should make the proper recursive calls", function() {
+  it("makes the proper recursive calls", function() {
     var addUpTo = sinon.spy(window, "addUpTo");
     addUpTo([1, 4, 5, 3], 2);
     expect(addUpTo.callCount).toEqual(3);
@@ -83,11 +83,11 @@ describe('addUpTo()', function() {
 });
 
 describe('maxOf()', function() {
-  it("should find the maximum integer in an array", function() {
+  it("finds the largest integer in an array", function() {
     expect(maxOf([1, 4, 5, 3])).toEqual(5);
   });
 
-  it("should make the proper recursive calls", function() {
+  it("makes the proper recursive calls", function() {
     var maxOf = sinon.spy(window, "maxOf");
     maxOf([1, 4, 5, 3]);
     expect(maxOf.callCount).toEqual(4);
@@ -96,15 +96,15 @@ describe('maxOf()', function() {
 
 
 describe('includesNumber()', function() {
-  it("should return true if the number is included in the array", function() {
+  it("returns 'true' if the number is present in the array", function() {
     expect(includesNumber([1, 4, 5, 3], 5)).toEqual(true);
   });
 
-  it("should return false if the number is not included in the array", function() {
+  it("returns 'false' if the number is not present in the array", function() {
     expect(includesNumber([1, 4, 2, 3], 5)).toEqual(false);
   });
 
-  it("should make the proper recursive calls", function() {
+  it("makes the proper recursive calls", function() {
     var includesNumber = sinon.spy(window, "includesNumber");
     includesNumber([1, 4, 5, 3], 3);
     expect(includesNumber.callCount).toEqual(4);
