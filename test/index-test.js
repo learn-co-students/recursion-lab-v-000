025476b847cb54671163aba5e8e0edafb6d80e1f
Gabe Jackson <gj@mail.co.de>
2017-06-07 17:35:11 -0400

    Space out LoC in each test that has lines of multiple types (setup code, function invocations, expectations) to improve organization and readability of test suite

--- test/index-test.js
+++ test/index-test.js
@@ -38,7 +38,9 @@ try {
 
     it("recurses, calling itself once for each letter in the string", function() {
       var printString = sinon.spy(window, "printString");
+
       printString("pizza");
+
       expect(printString.callCount).toEqual(5);
     });
   });
@@ -51,7 +53,9 @@ try {
 
     it("makes the proper recursive calls", function() {
       var reverseString = sinon.spy(window, "reverseString");
+
       reverseString("pizza");
+
       expect(reverseString.callCount).toEqual(5);
     });
   });
@@ -67,7 +71,9 @@ try {
 
     it("makes the proper recursive calls", function() {
       var isPalindrome = sinon.spy(window, "isPalindrome");
+
       isPalindrome("madamimadam")
+
       expect(isPalindrome.callCount).toEqual(6)
     });
   });
@@ -80,7 +86,9 @@ try {
 
     it("makes the proper recursive calls", function() {
       var addUpTo = sinon.spy(window, "addUpTo");
+
       addUpTo([1, 4, 5, 3], 2);
+
       expect(addUpTo.callCount).toEqual(3);
     });
   });
@@ -93,7 +101,9 @@ try {
 
     it("makes the proper recursive calls", function() {
       var maxOf = sinon.spy(window, "maxOf");
+
       maxOf([1, 4, 5, 3]);
+
       expect(maxOf.callCount).toEqual(4);
     });
   });
@@ -109,7 +119,9 @@ try {
 
     it("makes the proper recursive calls", function() {
       var includesNumber = sinon.spy(window, "includesNumber");
+
       includesNumber([1, 4, 5, 3], 3);
+
       expect(includesNumber.callCount).toEqual(4);
     });
   });
