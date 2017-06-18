a5fdbf28b623528f568e44ffe20d4228d5abc621
Mdjurdj1 <mdjurdj1@gmail.com>
2017-06-17 22:43:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 // Code your solution here!
+function printString(string) {
+  console.log(string[0])
+  if (string.length > 1) {
+    let substring = string.substring(1, string.length)
+    printString(substring)
+  } else {
+    return true
+  }
+}
 
+function reverseString(myString) {
+  if (myString.length < 2) {
+    return myString;
+  } else {
+    return reverseString(myString.substring(1)) + myString[0];
+  }
+}
+
+function isPalindrome(string) {
+  
+}
