425b5e550d4bd9699f41261b4eb19b5b062dd159
Elias Ventura <eliasventura23@gmail.com>
2017-08-09 18:53:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // Code your solution here!
-
+function printString(myString) {
+  let substring;
+  console.log(myString[0]);
+    if(myString.length > 1) {
+      substring= myString.substring(1, myString.length);
+      printString(substring)
+  } else {
+    return true;
+  }
+}
