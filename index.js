8bd0e0154bc660881873d9690552545463f26a35
Elias Ventura <eliasventura23@gmail.com>
2017-08-10 16:51:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 function printString(myString){
     let substring;
     console.log(myString[0]);
+    if(myString.length > 1) {
     substring= myString.substring(1, myString.length);
     printString(substring);
   } else {
     return true;
   }
+}
+
+function reverseString(myString) {
+
+
+  var revString= myString.split("").reverse().join("");
+  return revString
+
+
+}
