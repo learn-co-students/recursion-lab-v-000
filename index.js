4a0e32fd0bfa07355ba9403758f8ccd233a9c3d5
Nathan Robbert <admin@bigrocketgames.com>
2017-10-09 20:27:08 -0700

    Working on recursive functions.

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 // Code your solution here!
 
+function printString(string) {
+  console.log(string[0]);
+
+  if (string.length > 1) {
+    let substring = string.substring(1, string.length);
+    printString(substring);
+  } else {
+    return true;
+  }
+}
+
+function reverseString(string) {
+  if (string.length < 2) {
+    return string;
+  } else {
+    return reverseString(string.substring(1)) + string[0];
+  }
+}
+
+function isPalindrome(string) {
+  
+}
