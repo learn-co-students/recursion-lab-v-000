2ef8183271a4ef4533a592e079b38622350b156f
Nathan Robbert <admin@bigrocketgames.com>
2017-10-10 19:06:16 -0700

    Done.

--- index.js
+++ index.js
@@ -19,6 +19,36 @@ function reverseString(string) {
   }
 }
 
-function isPalindrome(string) {
-  
+function isPalindrome(myString) {
+  let l = myString.length;
+
+  if (l < 2) {
+    return true;
+  } else if (myString[l - 1] === myString[0]) {
+    return isPalindrome(myString.substring(1, l - 1));
+  } else {
+    return false;
+  }
+}
+
+function addUpTo(myArray, index) {
+  return index ? myArray[index] + addUpTo(myArray, --index) : myArray[index];
+}
+
+function maxOf(myArray) {
+  if (myArray.length === 1) {
+    return myArray[0];
+  } else {
+    return Math.max(myArray.pop(), maxOf(myArray));
+  }
+}
+
+function includesNumber(myArray, myNumber) {
+  if (!myArray.length) {
+    return false;
+  } else if (myArray[0] === myNumber) {
+    return true;
+  } else {
+    return includesNumber(myArray.slice(1), myNumber);
+  }
 }
