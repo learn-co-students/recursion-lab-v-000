8a579084e5e031ebc7fa947a539196d1da0df6f0
machen2 <victoriamachen@gmail.com>
2017-10-29 20:05:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 // Code your solution here!
 
+function printString(myString) 
+{
+    console.log(myString[0]);
+
+    if (myString.length > 1) {
+        let mySubstring = myString.substring(1, myString.length);
+        printString(mySubstring);
+    } else {
+        return true;
+    }
+}
+
+function reverseString(myString) 
+{
+    if (myString.length < 2) {
+        return myString;
+    } else {
+        return reverseString(myString.substring(1)) + myString[0];                
+    }
+}
+
+function isPalindrome(myString) 
+{
+    if (myString.length <= 1) {
+        return true;
+    } else if (myString[0] === myString[myString.length-1]) {
+        return isPalindrome(myString.substring(1, myString.length-1));            
+    } else {
+        return false;
+    }
+}
