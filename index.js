02555517a3923ebed0725fd8a18c9e0d174bba21
Sophia Hubeny <shhubeny@gmail.com>
2017-12-14 19:39:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 // Code your solution here!
 
+function printString(string){
+  console.log(string[0])
+  if (string.length > 1) {
+    let substring
+    substring = string.substring(1,string.length)
+    printString(substring)
+  } else {
+    return true
+  }
+}
+
+function reverseString(string){
+  if (string.length > 1) {
+    let substring = string.substring(0, string.length-1)
+    return string.charAt(string.length-1) + reverseString(substring)
+  } else {
+    return string.charAt(0)
+  }
+}
+
+function isPalindrome(){
+
+}
+
+function addUpTo(){
+
+}
+
+function maxOf(){
+
+}
+
+function includesNumber(){
+
+}
