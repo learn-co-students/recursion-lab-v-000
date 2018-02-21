db5c05f4ec14ae475758cc7aaee54c1ef9fcb813
Adam Tropp <adt6261@gmail.com>
2018-02-21 20:51:09 +0000

    Done.

--- index.js
+++ index.js
@@ -9,15 +9,45 @@ function printString(myString) {
   }
 }
 
-function reverseString(string) {
-  let subString = "";
-  let first_letter = string[0];
-  subString += first_letter
-  if (string.length > 0) {
-    debugger;
-    string -= first_letter
-    reverseString(subString)
+function reverseString(myString) {
+  if (myString.length < 2) {
+    return myString;
   } else {
-    return subString
+    return reverseString(myString.substring(1)) + myString[0];
   }
+}
+
+function isPalindrome(myString) {
+ let l = myString.length;
+
+ if (l < 2) {
+   return true;
+ } else if (myString[l - 1] === myString[0]) {
+   return isPalindrome(myString.substring(1, l - 1));
+ } else {
+   return false;
  }
+}
+
+function addUpTo(arr, i) {
+  return i ? arr[i] + addUpTo(arr, --i) : arr[i];
+}
+
+function maxOf(myArray) {
+  if (myArray.length === 1) {
+    return myArray[0];
+  } else {
+    return Math.max(myArray.pop(), maxOf(myArray));
+  }
+}
+
+function includesNumber(arr, num) {
+  if (arr.length === 1 && arr[0] !== num) {
+    return false;
+  } else if (arr[0] === num) {
+      return true;
+  } else {
+    arr = arr.slice(1);
+    return includesNumber(arr, num);
+  }
+}
