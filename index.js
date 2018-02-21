3f8cd62aae6ebd926a044abfaf190fd03c942e3e
Adam Tropp <adt6261@gmail.com>
2018-02-21 08:05:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-// Code your solution here!
+function printString(myString) {
+  let mySubstring;
+  console.log(myString[0]);
+  if (myString.length > 1) {
+    mySubstring = myString.substring(1, myString.length);
+    printString(mySubstring);
+  } else {
+    return true;
+  }
+}
 
+function reverseString(string) {
+  let subString = "";
+  let first_letter = string[0];
+  subString += first_letter
+  if (string.length > 0) {
+    debugger;
+    string -= first_letter
+    reverseString(subString)
+  } else {
+    return subString
+  }
+ }
