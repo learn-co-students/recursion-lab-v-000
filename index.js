1cb57883372c90eef8d715f46fac18adf39902b6
github username <github email address>
2018-04-20 19:24:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
-// Code your solution here!
+function printString(myString) {
+  console.log(myString[0]);
 
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}
+
+function reverseString(string) {
+    if (string.length < 2) {
+      return string;
+    } else {
+      return reverseString(string.substring(1)) + string[0];
+    }
+  }
+
+function isPalindrome(string) {
+
+}
+
+function addUpto(string) {
+
+}
+
+function maxOf(string) {
+
+}
+
+function includesNumber(string) {
+
+}
