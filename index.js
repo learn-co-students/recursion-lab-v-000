bd10b66fb79de99fd48563f05f73569f7ceab694
MargaritaKhodzka <mkhodzka@gmail.com>
2018-05-28 00:26:04 -0400

    printString and reverseString functions

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 // Code your solution here!
 
+function printString (myString) {
+  console.log(myString[0]);
+
+  if (myString.length > 1) {
+    let subString = myString.substring(1, myString.length);
+    printString(subString);
+  } else {
+    return true;
+  }
+}
+
+//printString('pizza')
+//printString("supercalifragilisticexpialidocious");
+
+function reverseString(string) {
+  if (string.length > 1) {
+    return string.charAt(string.length - 1) + reverseString(string.substring(string.length - 1, 0))
+  } else {
+    return string
+  }
+}
