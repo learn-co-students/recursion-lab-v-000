0caf473165a64bccc45b403ea319f3c5d70a8c80
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-07-03 19:19:22 -0400

    Implement recursive reverseString method

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ function printString(string){
         printString(string.slice(1))
     }
 }
+
+function reverseString(string){
+    if (1 === string.length){
+        return string;
+    }
+    else {
+        const lastIndex = string.length -1 
+        return string[lastIndex] + reverseString(string.slice(0, lastIndex))
+    }
+}
