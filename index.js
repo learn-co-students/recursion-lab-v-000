49b6da19ecf335fbc929bfdb1a89bbb8835b5ee3
Bret <Burhooo@gmail.com>
2018-09-19 05:59:57 -0500

    isPalindrome isWorking

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 // Code your solution here!
 
+function printString(myString) {
+    console.log(myString[0])
+    if(myString.length > 1) {
+        myString = myString.substring(1, myString.length)
+        printString(myString)
+    } else {
+        return true
+    }   
+}
+
+function reverseString(myString) {
+    if (myString === '') {
+        return myString
+    } else {
+        return reverseString(myString.substring(1)) + myString.charAt(0)
+    }
+}
+
+function isPalindrome(myString) {
+    if(myString.length === 1 || myString.length === 0) {
+        return true
+    } else if(myString[0] === myString[myString.length - 1]) {
+        return isPalindrome(myString.slice(1, myString.length - 1))
+    } else {
+        return false
+    }
+}
