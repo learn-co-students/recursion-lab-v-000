9000aed19560a394f06f90c23821234e91248fa2
Dillorom <dabdulloeva@gmail.com>
2018-11-05 10:14:24 -0800

    Done.

--- index.js
+++ index.js
@@ -11,55 +11,48 @@ function printString(myString){
     }
 }
 
-
-
 function reverseString(str) {
     if (str === "") 
       return "";
-    
     else
       return reverseString(str.substr(1)) + str.charAt(0);
 }
 
-// function isPalindrome(str){
-//     if (printString(str) == reverseString(str)){
-//         return true
-//     } 
-// }
-
 function isPalindrome(str) {
-    var len = Math.floor(str.length / 2);
-    for (var i = 0; i < len; i++)
-      if (str[i] !== str[str.length - i - 1])
-        return false;
-    return true;
-  }
-  function addUpTo(arr){
-  
-    if (arr.length == 1) return arr[0] // base case
-    return arr[0] + addUpTo(arr.slice(1))  // recurse
-    
-  }
-
-  function maxOf(arr){
-    var max = 0;
-    
-    for (var i = 0; i < arr.length; i++)
-    {
-        if (arr[i] > max)
-        {
-            max = arr[i];
-        }
+    const strLen = str.length;
+    if (strLen === 0 || strLen === 1) {
+        return true;
     }
-    return max;
+    
+    if (str[0] === str[strLen - 1]) {
+        return isPalindrome( str.slice(1, strLen - 1) );
+    }
+    
+    return false;
+};
 
-  }
+function addUpTo(myArray, index) {
+    return index ? myArray[index] + addUpTo(myArray, --index) : myArray[index];
+}
 
-  function includesNumber(arr, num){
-      for (var i = 0; i < arr.length; i++){
-          if (arr[i] == num){
-              return true;
-          } 
-      }
+function maxOf(arr) {
+    var num = arr.slice();
+    if (num.length == 1) {
+        return	num[0];
+    }
+    if (num[0] < num[1]) { num.splice(0,1); }
+    else { num.splice(1,1); }
 
-  }
+// with one less number in the array, call the same function
+    return maxOf(num);
+}
+
+function includesNumber(arr, num){
+    if (!arr.length){
+        return false
+    } else if (arr[0] === num){
+            return true;
+    } else { 
+        return includesNumber(arr.slice(1), num)
+    }
+}
