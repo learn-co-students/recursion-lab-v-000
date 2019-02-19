8f84456abb75866d7c1193aea909923653d257e8
sgibson47 <sgibson47@gmail.com>
2019-02-19 16:22:11 -0600

    add how to steps as comments

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-// Code your solution here!
+// 1. Apply the problem to a specific case (i.e., choose an example).
+// 2. Write out a function that solves that particular example.
+// 3. Reword that function so that it uses recursion, invoking itself.
+
+
+
 
