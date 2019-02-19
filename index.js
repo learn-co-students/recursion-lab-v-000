697fdcd7c6ab7a58083dc25ca69d1199b6ed2ed0
sgibson47 <sgibson47@gmail.com>
2019-02-19 16:27:55 -0600

    rewrite function to call itself

--- index.js
+++ index.js
@@ -4,6 +4,21 @@
 
 
 function printString(myString) {
-
+  // 1. 
+  // mystring = "food"
+  // 2.
+  // console.log(myString[0], myString[1], myString[2], myString[3])
+  // 3.
+  let substring;
+ 
+  // Print out the current first character in the string.
+  console.log(myString[0]);
+ 
+  // Store the remainder of the string in the 'substring' variable.
+  substring = myString.substring(1, myString.length);
+ 
+  // Invoke printString() from within, passing in the remainder of the previous string.
+  printString(substring);
+  
 }
 
