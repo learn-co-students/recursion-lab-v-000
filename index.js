ecae0ff030a6a647d3e723be2df0f1f9cff84dc7
VictoriaMeng <1ststarofthenight@gmail.com>
2019-02-19 17:14:24 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+import { reverse } from "dns";
+
 // Code your solution here!
 
+function printString(myString) {
+  console.log(myString[0])
+  if (myString.length > 1) {
+    let mySubstring = myString.substring(1, myString.length);
+    printString(mySubstring)
+  }
+}
+
+function reverseString(myString) {
+  if (myString === '') {
+    return "";
+  } else {
+    return reverseString(myString.substr(1)) + myString.charAt(0)
+  }
+}
+
+function isPalindrome() {
+  
+}
