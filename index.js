0e5594e5ed5307d0d1d9be38a3aa39caba879d0d
Olena Ageyeva <eageeva@hotmail.com>
2019-03-17 02:30:44 +0000

    printString

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-// Code your solution here!
-
+// Code your solution
+function printString(myString){
+  console.log(myString[0])
+  if (myString.length>1){
+    printString(myString.substring(1,myString.length))
+  }else{
+    return true
+  }
+}
