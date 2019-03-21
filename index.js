5a4c3c05fb030fb20a8c4acdcdeed1528839935d
Paris Dolfman <parisdolfman@gmail.com>
2019-03-21 17:44:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // Code your solution here!
 
+function printString(string) {
+  console.log(string[0]);
+  if (string.length > 1) {
+    let subString =
+    string.substring(1, string.length);
+    printString(subString);
+  } else {return true;
+  }
+}
