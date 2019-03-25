a0c9aae4d3c4cb2222c4785c539cb103f3c9ad80
Paris Dolfman <parisdolfman@gmail.com>
2019-03-25 15:39:20 +0000

    Done.

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ function reverseString(string) {
   if (string.length < 2) {
     return string;
   } else {
-    return reverseString(string.substring(1)) + string[0]
+    return reverseString(string.substring(1)) + string[0];
   }
+}
+
+function isPalindrome(myString) {
+  let count = myString.length;
+
+  if (count < 2) {
+    return true;
+  } else if (myString[count - 1] === myString[0]) {
+    return isPalindrome(myString.substring(1, count - 1));
+  } else {
+    return false;
+  }
+}
+
+function addUpTo(array, index) {
+  return index ? array[index] + addUpTo(array, --index) : array[index]
 }
