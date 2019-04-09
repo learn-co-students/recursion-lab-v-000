5ab2fc6c3b218c4234af0e094faf00d2a2d46904
rcrdlbl <bluh@outlook.com>
2019-04-09 17:21:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // Code your solution here!
+function printString(myString) {
+  console.log(myString[0])
 
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length)
+    printString(mySubString)
+  } else {
+    return true
+  }
+}
