39f0ccd7920878300dc765ef171fd92ff5127c10
rcrdlbl <bluh@outlook.com>
2019-04-09 17:45:00 -0400

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ function printString(myString) {
     return true
   }
 }
+
+function reverseString(string) {
+  if (string.length > 1) {
+    return reverseString(string.substring(1)) + string[0]
+  } else {
+    return string
+  }
+}
+
+function isPalindrome(string) {
+  if (string.length < 2) {
+    return true
+  }
+
+  if (string.slice(0, 1) == string.slice(-1)) {
+    return isPalindrome(string.slice(1, -1))
+  }
+
+  return false
+}
