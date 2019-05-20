ad89848be92306cafbddc84befeadd9831eaaf01
mseaman6 <monicaameeti@gmail.com>
2019-05-20 09:24:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-// Code your solution here!
+function printString(myString) {
+  console.log(myString[0]);
 
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}
+
+let reversedString = ''
+
+function reverseString(myString) {
+  reversedString += myString[myString.length-1];
+  console.log(reversedString);
+  if (myString.length > 1) {
+    let mySubString = myString.substring(0, myString.length-1);
+    reverseString(mySubString);
+  } else {
+    return reversedString;
+  }
+}
