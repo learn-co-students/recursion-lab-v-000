3b586d2e27bb27602cadbde46a5d84b1f1cace5e
Nate Miller <natekmiller96@gmail.com>
2019-05-29 18:53:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,73 @@
-// Code your solution here!
+function printString(myString) {
+  console.log(myString[0]);
+ 
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}
 
+function reverseString(myString) {
+ 
+  if (myString.length > 1) {
+    return reverseString(myString.substr(1)) + myString.charAt(0);
+  } else {
+    return myString;
+  }
+}
+
+function isPalindrome(myString) {
+ 
+  if (myString.length > 1) {
+    let first = myString.charAt(0);
+    let last = myString.charAt(myString.length - 1);
+    if (first == last){
+      return isPalindrome(myString.substr(1, myString.length - 2));
+    }
+    else {
+      return false;
+    }
+  } else {
+    return true;
+  }
+}
+
+function addUpTo(idx, n) {
+  if (n === 0){
+    return idx[n];
+  } else{
+    return addUpTo(idx, n - 1) + idx[n];
+  }
+}
+
+function maxOf(idx) {
+  if (idx.length > 1){
+    if(idx[0] < idx[idx.length - 1]){
+      idx[0] = idx[idx.length - 1];
+      idx.pop();
+      return maxOf(idx);
+    } else {
+      idx.pop();
+      return maxOf(idx);
+    }
+  } else {
+    return idx[0];
+  }
+}
+
+function includesNumber(idx, n) {
+  if (idx.length === 0){
+    return false;
+  }
+  else {
+    if (idx[0] === n){
+      return true;
+    }
+    else {
+      idx.shift();
+      return includesNumber(idx, n);
+    }
+  }
+}
