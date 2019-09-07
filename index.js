d2cf63295e1eea8f336ab52d74f2a19095a1c701
christianavdm <christianavdm@gmail.com>
2019-09-07 15:21:52 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 // Code your solution here!
 
+function printString(myString) {
+    console.log(myString[0]);
+   
+    if (myString.length > 1) {
+      let mySubString = myString.substring(1, myString.length);
+      printString(mySubString);
+    } else {
+      return true;
+    }
+  }
+
+  function reverseString(myString) {
+      let newString; 
+      if (myString.length > 1) {
+        let lastLetterIndex = myString.length - 1;
+        newString += myString[lastLetterIndex]; 
+        console.log (newString)
+        reverseString(myString.substring(0, lastLetterIndex))
+      } else {
+      console.log(newString)
+      return newString;
+      }
+  }
