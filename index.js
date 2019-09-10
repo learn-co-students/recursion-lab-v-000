9fdcb647c1f1b5df8894fd002acf3df4e3258efd
christianavdm <christianavdm@gmail.com>
2019-09-10 15:17:05 -0700

    Done.

--- index.js
+++ index.js
@@ -11,17 +11,47 @@ function printString(myString) {
     }
   }
 
+function reverseString(myString, reversedString ="") {
+  let lastLetter = myString.length - 1; 
 
-  function reverseString(myString) {
-    let lastLetterIndex = myString.length - 1; 
-    let newString;  
+  if (lastLetter > 0) {
+    let newReversedString = reversedString + myString[lastLetter]; 
+    let newString = myString.slice(0, lastLetter); 
+    reverseString(newString, newReversedString); 
+  } else {
+    let finalString = reversedString + myString; 
+    console.log(finalString); 
+    return finalString; 
+  }
+}
 
-    if (lastLetterIndex > 0) {
-        newString = newString + myString[lastLetterIndex]; 
-        let subString = myString.substring(0, lastLetterIndex); 
-        reverseString(subString); 
-    } else { 
-        newString = newString + myString; 
-        return newString; 
+function isPalindrome(myString) {
+  let palindrome; 
+  let lastLetterIndex = myString.length - 1; 
+
+  if (lastLetterIndex > 0) {
+    if (myString[0] === myString[lastLetterIndex]) {
+      let newString = myString.slice(1, lastLetterIndex); 
+      isPalindrome(newString); 
+    } else {
+      palindrome = false; 
     }
+  } else {
+    palindrome = true; 
+  }
+  console.log(palindrome)
+  return palindrome; 
+}
+
+function addUpTo(numberArray, numberIndex, sum =0) {
+  if (numberIndex > 0) {
+    let newSum = sum + numberArray[0]; 
+    let newArray = numberArray.slice(1)
+    let newIndex = numberIndex - 1; 
+    addUpTo(newArray, newIndex, newSum)
+  } else {
+    let totalSum = sum + numberArray[0]; 
+    console.log(totalSum); 
+    return totalSum; 
+  }
 }
