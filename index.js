5a5f53537e05ce5f11c9f8ab022dfd1dc120e1f5
Tom Cho <tc.thomascho@gmail.com>
2020-01-03 16:21:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,57 @@
 // Code your solution here!
+function reverseString(string) {
+  if (string.length > 1) {
+    let subString = string.substring(1, string.length);
+    return reverseString(subString) + string[0];
+  } else {
+    return string;
+  };
+};
 
+function isPalindrome(string) {
+  if (string.length > 1 && string[0] === string[string.length-1]) {
+    let subString = string.substring(1, string.length-1);
+    return isPalindrome(subString);
+  } else if (string.length > 1 && string[0] !== string[string.length-1]) {
+    return false;
+  } else {
+    return true;
+  };
+};
+
+function printString(string) {
+  if (string.length > 1) {
+    console.log(string[0]);
+    printString(string.substring(1, string.length));
+  } else {
+    console.log(string);
+  }
+};
+
+function addUpTo(array, index) {
+  if (index > 0) {
+    return addUpTo(array, index-1) + array[index];
+  } else {
+    return array[index];
+  };
+};
+
+function maxOf(myArray) {
+  console.log(myArray);
+  if (myArray.length === 1) {
+    return myArray[0];
+  } else {
+    return Math.max(myArray.pop(), maxOf(myArray));
+  };
+};
+
+function includesNumber(array, number) {
+  console.log(array);
+  if (array.shift() === number) {
+    return true
+  } else if (!array.length) {
+    return false
+  } else {
+    return includesNumber(array, number)
+  };
+};
