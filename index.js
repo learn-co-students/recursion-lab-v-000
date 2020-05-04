9a2d2faa4cfe298bf031c8c872fb5509d892efd4
ahotchkin <allyson.hotchkin@gmail.com>
2020-05-04 14:45:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,62 @@
 // Code your solution here!
 
+function printString(myString) {
+  console.log(myString[0]);
+
+  if (myString.length > 1) {
+    const mySubString = myString.substring(1, myString.length);
+    printString(mySubString);
+  } else {
+    return true;
+  };
+}
+
+function reverseString(myString) {
+  if (myString.length > 0) {
+    return reverseString(myString.substring(1)) + myString.charAt(0);
+  } else {
+    return myString;
+  };
+}
+
+function isPalindrome(myString) {
+  if (myString.length === 0 || myString.length === 1) {
+    return true;
+  } else if (myString[0] === myString[myString.length - 1]) {
+    return isPalindrome(myString.slice(1, myString.length - 1));
+  } else {
+    return false;
+  };
+}
+
+function addUpTo(myArray, index) {
+  let sum = 0;
+  if (index > 0) {
+    sum = addUpTo(myArray, index - 1) + myArray[index];
+  } else {
+    return sum + myArray[index];
+  };
+  return sum;
+}
+
+function maxOf(myArray) {
+  if (myArray.length > 1) {
+    myArray[0] < myArray[1] ? myArray.splice(0, 1) : myArray.splice(1, 1);
+    return maxOf(myArray);
+  } else {
+    return myArray[0];
+  };
+}
+
+function includesNumber(myArray, num) {
+  if (myArray.length > 1) {
+    if (num === myArray[0]) {
+      return true;
+    } else {
+      myArray.splice(0, 1);
+      return includesNumber(myArray, num);
+    };
+  } else {
+    return false;
+  };
+}
